96c68de05ee153917e2818c44100276ca0f9a9c8
rezajobs <120446719+rezajobs@users.noreply.github.com>
2024-05-20 22:37:25 -0400

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,4 +1,23 @@
-number1 = int(input('say a number : '))
-number2 = int(input('say a number : '))
+while True:
+    number1 = int(input('say a number : '))
+    number2 = int(input('say a number : '))
 
-print('Addition :',number1+number2,'Subtraction :', number1-number2,'Multiplication :', number1*number2,'Division :', number1/number2)
+    calculator = input('Choose an operation (+, -, *, /):')
+    if calculator == '+':
+        print('Addition :',number1+number2)
+    elif calculator == '-':
+        print('Subtraction :', number1-number2)
+    elif calculator == '*':
+        print('Multiplication :', number1*number2)
+    elif calculator == '/':
+        if number2 == 0:
+            print('Error: Division by zero is not allowed.')
+        else:
+                print('Division:', number1 / number2)
+    else:
+            print('Invalid operation. Please choose +, -, *, or /.')
+    print('Division :', number1/number2)
+
+    continue_calculation = input('Do you want to perform another calculation? (yes/no): ').strip().lower()
+    if continue_calculation != 'yes':
+         break
