99c567bc00c24f4eebd000c1c31c700b223e1b96
rezajobs <rezavaseghi39@gmail.com>
2024-05-20 21:53:26 -0400

    Update 'Calculator =  Calculator.py with new changes

---
+++ Simple Calculator.py
@@ -0,0 +1,4 @@
+number1 = int(input('say a number : '))
+number2 = int(input('say a number : '))
+
+print('Addition :',number1+number2,'Subtraction :', number1-number2,'Multiplication :', number1*number2,'Division :', number1/number2)
