480f2d9eba4492936373a7a1fbea04d8e88da380
rezajobs <rezavaseghi39@gmail.com>
2024-05-20 21:41:18 -0400

    Update 'p1= Untitled-1.py' with new changes

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,4 @@
-user = int(input)
+number1 = int(input('say a number : '))
+number2 = int(input('say a number : '))
+
+print('Addition :',number1+number2,'Subtraction :', number1-number2,'Multiplication :', number1*number2,'Division :', number1/number2)
